00ad75a123370d8e3f9e1f5bb524885a42e9112b
doyun0304 <doyun0304@naver.com>
2025-03-11 09:50:53 +0900

    submit

--- Math.fs
+++ Math.fs
@@ -6,4 +6,9 @@ open System
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
 let collatz n =
-  0
+  let rec f (t:uint64) result = 
+    if t=1UL then result
+    elif t%2UL=0UL then f (t/2UL) (result+1)
+    else f (3UL*t+1UL) (result+1)
+  
+  f (uint64 n) 0
